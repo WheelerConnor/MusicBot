7ab9238d9a501fc3c3f95495edf23b518cdb4a38
Connor <connorsteam742@gmail.com>
2016-11-09 20:14:33 -0500

    Add files via upload

---
+++ buildpack-run.sh
@@ -0,0 +1,10 @@
+sudo apt-get install build-essential unzip -y
+sudo apt-get install software-properties-common -y
+sudo add-apt-repository ppa:fkrull/deadsnakes -y
+sudo add-apt-repository ppa:mc3man/trusty-media -y
+sudo add-apt-repository ppa:chris-lea/libsodium -y
+sudo add-apt-repository ppa:mc3man/xerus-media -y
+sudo add-apt-repository ppa:mc3man/trusty-media -y
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install git python3.5 python3.5-dev ffmpeg libopus-dev libffi-dev libsodium-dev -y
