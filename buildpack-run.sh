da60f461ee10c6f8eb35a07d4176b9d12c479906
Connor <connorsteam742@gmail.com>
2016-11-09 20:18:16 -0500

    Update buildpack-run.sh

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,10 +1,10 @@
-sudo apt-get install build-essential unzip -y
-sudo apt-get install software-properties-common -y
-sudo add-apt-repository ppa:fkrull/deadsnakes -y
-sudo add-apt-repository ppa:mc3man/trusty-media -y
-sudo add-apt-repository ppa:chris-lea/libsodium -y
-sudo add-apt-repository ppa:mc3man/xerus-media -y
-sudo add-apt-repository ppa:mc3man/trusty-media -y
-sudo apt-get update -y
-sudo apt-get upgrade -y
-sudo apt-get install git python3.5 python3.5-dev ffmpeg libopus-dev libffi-dev libsodium-dev -y
+apt-get install build-essential unzip -y
+apt-get install software-properties-common -y
+add-apt-repository ppa:fkrull/deadsnakes -y
+add-apt-repository ppa:mc3man/trusty-media -y
+add-apt-repository ppa:chris-lea/libsodium -y
+add-apt-repository ppa:mc3man/xerus-media -y
+add-apt-repository ppa:mc3man/trusty-media -y
+apt-get update -y
+apt-get upgrade -y
+apt-get install git python3.5 python3.5-dev ffmpeg libopus-dev libffi-dev libsodium-dev -y
